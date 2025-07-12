4d51585a6a9ff3b9d2df394b96baebc69f1b6151
Parsa <parsa.ashrafi83@gmail.com>
2025-07-12 18:47:42 +0330

    Add files via upload

---
+++ Telegram Bot/ppt or docx to pdf/Token.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TelegramBot
+{
+    public static class Token
+    {
+        private const string token = "your-token";
+
+        public static string GetToken()
+        {
+            return token;
+        }
+    }
+}
